802ea2164dee2b2b4d7d2b5aebbe5dd6e7e1606d
Antonio <antoniomaestregarrido.com>
2021-11-19 13:22:35 +0100

    created card function

---
+++ src/dom.js
@@ -0,0 +1,13 @@
+
+
+export function testA(){
+    console.log("A");
+
+}
+export function testB() {
+    console.log("B")
+    
+}function qazqaz() {
+    console.log("B")
+    
+}
--- src/main.js
+++ src/main.js
@@ -1,36 +1,95 @@
 
-var projectList=[];
-var i=0;
+import * as dom from "./dom.js";
+
+dom.testA()
+var projectList = [];
+var i = 0;
 
 
 
 
 class Project {
-    
-    constructor(nombre, categoria, dueDate) {
+
+    constructor(nombre, description, categoria) {
         this.nombre = nombre
-        this.categoria = categoria
-        this.dueDate = dueDate
-        this.taskList=[];
-        this.id=++i;
-       
-          }
+        this.dueDate = this.dueDate
+        this.description = description
+        
+        this.toDoList = [];
+        this.id = ++i;
+
+    }
+    addToDo(todo) {
+        this.toDoList.push(todo)
+
+        
+    }
 
 
-  
+
 }
+class toDo {
+
+    constructor(nombre, description, dueDate) {
+        this.nombre = nombre
+       
+        this.description = description
+        this.dueDate = dueDate
+        
+        
+
+    }
+
+
+
+}
+
+
+
+function createCard() {
+    for (let i=0;i<projectList.length;i++){
+        var cardElement=projectList[i];
+        var f=new DocumentFragment
+
+        const card=document.createElement("div");
+        card.classList.add("card")
+        
+        const   cardName=document.createElement("h4");
+        card.appendChild(cardName);
+        cardName.innerHTML=cardElement.nombre;
+
+        const cardDueDate=document.createElement("p");
+        cardDueDate.innerHTML=cardElement.dueDate;
+        card.appendChild(cardDueDate)
+
+        const cardDescription=document.createElement("p");
+        cardDescription.innerHTML=cardElement.description
+        card.appendChild(cardDescription);
+
+        card.addEventListener("click",()=>alert("click"))
+
+        document.querySelector("#panel").appendChild(card);
+        
+
+    }
+    
+}
+
 dummydata();
 console.log(projectList)
+createCard()
 
 
 
-function dummydata(){
-    var uno=new Project("the odin project","coding","31/12/2022")
-    var dos=new Project("black belt","bjj","31/12/2023")
+function dummydata() {
+    var uno = new Project("the odin project", "pues eso", "31/12/2022")
+    var dos = new Project("black belt", "a ver si llego",  "31/12/2023")
     projectList.push(uno);
     projectList.push(dos);
-
-
+    var dos = new toDo("black belt", "a ver si llego", "bjj", "31/12/2023")
+    var sdd = new toDo("black belsdsdt", "a ver si llego", "bjj", "31/12/2023")
+    uno.addToDo(dos)
+    uno.addToDo(sdd)
 
 
 }
